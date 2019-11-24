19348b680af4028f5e2334522e2185474d50fc12
chithsuthway81 <chithsuthway81@gmail.com>
2019-11-24 11:10:51 +0630

    movie-app

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import vuetify from "./plugins/vuetify";
+import App from "./App.vue";
+import { store } from "./store";
+import router from "./router";
+import axios from "axios";
+
+Vue.config.productionTip = false;
+axios.defaults.baseURL =
+  "http://www.omdbapi.com/?apikey=b76b385c&page=1&type=movie&Content-Type=application/json";
+new Vue({
+  render: h => h(App),
+  vuetify,
+  store,
+  router
+}).$mount("#app");
